fdba44de6ba5ffffe8e8e298bb9d20c74fabaee9
mncc8337 <mncc8327@gmail.com>
2024-01-06 22:48:19 +0700

    fix date in some scripts

--- .config/awesome/scripts/screenshot.sh
+++ .config/awesome/scripts/screenshot.sh
@@ -2,14 +2,15 @@ savedir=''
 prefix=''
 cwd=$(pwd)
 if [ "$2" = 'save' ]; then
-    savedir="$(date +"%m-%d-%y_%H-%M-%S").png "
-    prefix=" and saved to ~/Pictures/$(date +"%m-%d-%y_%H-%M-%S").png"
+    savedir="$(date +"%Y-%m-%d %H-%M-%S").png"
+    prefix=" and saved to ~/Pictures/$savedir"
+    savedir="\"$savedir\""
     cd ~/Pictures
 fi
 if [ "$1" = 'full' ]; then
-    maim $savedir--hidecursor | xclip -selection clipboard -t image/png
+    maim $savedir --hidecursor | xclip -selection clipboard -t image/png
 elif [ "$1" = 'area' ]; then
-    maim -s $savedir--hidecursor | xclip -selection clipboard -t image/png
+    maim -s $savedir --hidecursor | xclip -selection clipboard -t image/png
 fi
 
 notify-send "<b>Screen captured</b><br>Image copied to clipboard$prefix"
